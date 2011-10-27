384e266e65dbbbc22130f001104ed702cdda3cf6
Willi Schinmeyer <willi@schinmeyer.de>
2011-10-27 23:37:18 +0200

    Started work on the UMenu Unit containing a menu class which I will use for the main and game over menu later. (And could easily use for a hiscore and all kinds of menus.) The Head/Body duality leads to a lot of duplicate code, I should add another class for those. (Why didn't I think of this earlier?) I ran into some problems regarding arrays and currently have no internet so I cannot look for solutions. I hope the net will get fixed before deadline, this could kill me. Test.pas contains simple code for reproducing the problem.

--- Test.pas
+++ Test.pas
@@ -7,7 +7,20 @@
  * Contains various tests.
  *)
 
-uses UGeneralConstants;
-
+procedure writeLines(var lines : array of string);
+var
+	i : integer;
+	dynArray : array of string;
 begin
+	dynArray := lines;
+	for i := low(dynArray) to high(dynArray) do
+		writeln(dynArray[i]);
+end;
+
+var
+	lines : array[0..3] of string = ('hello,', 'beautiful', 'array', 'world!');
+	nolines : array of string;
+begin
+	writeLines(lines);
+	//writeLines(nolines);
 end.
